ec9e1a874ecdefaa6f93a6030330224851116a5b
Felipe-Mina <88908796+Felipe-Mina@users.noreply.github.com>
2023-03-14 16:30:40 -0300

    Lista de productos agregada

--- api/src/controllers/productControllers.js
+++ api/src/controllers/productControllers.js
@@ -1,12 +1,10 @@
  const axios = require('axios')
- const productos = require('./products.json')
  const e = require('express')
  const {Productos} = require('../db')
 
-const getProducts = async (req, res) => {
-    console.log(productos)
-    res.status(200).send(productos)
-}
+
+
+
 
 module.exports = {
     getProducts
---
+++ api/src/controllers/utils.js
@@ -0,0 +1,47 @@
+const { default: axios } = require("axios");
+const { Productos } = require("../db");
+const Sequelize = require("sequelize");
+const e = require("express");
+const productos = require('./products.json')
+const Op = Sequelize.Op;
+require("dotenv").config();
+
+const getProducts = async (req, res) => {
+    console.log(productos)
+    res.status(200).send(productos)
+}
+
+const getAll = async (req, res) => {
+    const db = await Productos.findAll();
+    if (!db[0]) {
+        productos.map(async e => {
+            await Productos.create({
+                nombre: e.nombre,
+                imagen: e.imagen,
+                precio: e.precio
+            });
+        });
+    }
+    const allProducts = await Productos.findAll()
+    res.status(200).send(allProducts)
+}
+
+const createProduct = async (req, res) => {
+    let {nombre, imagen, precio} = req.body;
+    try {
+        const dbProduct = await Productos.create({
+            nombre: nombre,
+            imagen: imagen,
+            precio: precio
+        });
+        res.status(200).send(dbProduct)
+    } catch(error) {
+        console.log(error)
+    }
+};
+
+module.exports = {
+    createProduct,
+    getAll,
+    getProducts
+}
