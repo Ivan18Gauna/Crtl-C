6ba000b12005ab08fa9670c77b5562f814ce010b
Felipe-Mina <88908796+Felipe-Mina@users.noreply.github.com>
2023-03-15 10:52:56 -0300

    Ruta de busqueda por nombre creada

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,13 +2,15 @@ const axios = require('axios')
 const { Router } = require('express');
 require('dotenv').config();
 const {Productos} = require('../db')
-const {getProducts, createProduct, getAll} = require('../controllers/utils')
+const {getProducts, createProduct, getAll, getByName} = require('../controllers/utils')
 
 const router = Router();
 
 router.get('/products', getProducts)
 
 router.get('/all', getAll)
+
+router.get('/products/:name', getByName)
 
 router.post('/agregarProducto', createProduct)
 
