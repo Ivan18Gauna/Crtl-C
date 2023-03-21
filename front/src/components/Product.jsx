39791d3a8235ad47d986b95934b95a18471563bd
ivan18gauna <ivan18gauna@gmail.com>
2023-03-21 14:53:36 -0300

    products

--- front/src/components/Product.jsx
+++ front/src/components/Product.jsx
@@ -1,51 +1,33 @@
 import { Link } from "react-router-dom";
+import { products } from "./array";
 
 export function Product() {
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 pt-12 place-items-center h-screen px-20 gap-12">
-      {/* <div className="flex flex-col md:flex-row md:space-x-4 py-10 pb-22 lg:px-42 md:px-20 text-center"> */}
-      <div className="bg-gray-100 rounded-lg shadow-2xl p-4 w-full">
-        <Link>
-          <h2 className="text-xl font-semibold mb-7 flex justify-center items-center">Camperas</h2>
-          <img
-            className="hover:transform hover:scale-110 transition duration-300 ease-in-out rounded-2xl"
-            src="https://th.bing.com/th/id/R.3e40f90f3210efe682c9619a89c7c4e2?rik=CCyra%2bvdmDc1tA&pid=ImgRaw&r=0"
-            alt=""
-          />
-        </Link>
-      </div>
-      <div className="bg-gray-100 rounded-lg shadow-2xl p-4 w-full">
-        <h2 className="text-xl font-semibold mb-7 flex justify-center items-center">Mochilas</h2>
-        <img
-          className="hover:transform hover:scale-110 transition duration-300 ease-in-out rounded-2xl"
-          src="https://th.bing.com/th/id/R.3e40f90f3210efe682c9619a89c7c4e2?rik=CCyra%2bvdmDc1tA&pid=ImgRaw&r=0"
-          alt=""
-        />
-      </div>
-      <div className="bg-gray-100 rounded-lg shadow-2xl p-4 w-full">
-        <h2 className="text-xl font-semibold mb-7 flex justify-center items-center">Zapatillas</h2>
-        <img
-          className="hover:transform hover:scale-110 transition duration-300 ease-in-out rounded-2xl"
-          src="https://th.bing.com/th/id/R.3e40f90f3210efe682c9619a89c7c4e2?rik=CCyra%2bvdmDc1tA&pid=ImgRaw&r=0"
-          alt=""
-        />
-      </div>
-      <div className="bg-gray-100 rounded-lg shadow-2xl p-4 w-full">
-        <h2 className="text-xl font-semibold mb-7 flex justify-center items-center">Pantalones</h2>
-        <img
-          className="hover:transform hover:scale-110 transition duration-300 ease-in-out rounded-2xl"
-          src="https://th.bing.com/th/id/R.3e40f90f3210efe682c9619a89c7c4e2?rik=CCyra%2bvdmDc1tA&pid=ImgRaw&r=0"
-          alt=""
-        />
-      </div>
-      <div className="bg-gray-100 rounded-lg shadow-2xl p-4 w-full">
-        <h2 className="text-xl font-semibold mb-7 flex justify-center items-center">Accesorios</h2>
-        <img
-          className="hover:transform hover:scale-110 transition duration-300 ease-in-out rounded-2xl"
-          src="https://th.bing.com/th/id/R.3e40f90f3210efe682c9619a89c7c4e2?rik=CCyra%2bvdmDc1tA&pid=ImgRaw&r=0"
-          alt=""
-        />
-      </div>
+      {products.map((el) => {
+        return (
+          <div className="bg-gray-100 rounded-lg shadow-2xl p-4 w-full h-full flex justify-center items-center">
+            <Link>
+              <h2 className="text-xl font-semibold mb-7 flex justify-center items-center">
+                {el.nombre}
+              </h2>
+                <img
+                  className="hover:transform hover:scale-110 transition duration-300 ease-in-out rounded-2xl h-96 object-cover"
+                  src={el.imagen}
+                  alt=""
+                />
+                <div className="flex justify-between pt-6">
+                  <div>${el.precio}</div>
+                  <div>
+                    {
+                      el.stock? 'Disponible': 'Sin Stock'
+                    }
+                  </div>
+                </div>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
